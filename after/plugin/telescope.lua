9c77a7baf5e960d498245b49b655b43e60b5e19c
Den Tsou <den3tsou@gmail.com>
2023-03-12 09:23:54 +1100

    don't store packer_compiled.lua

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>p', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
+-- vim.keymap.set('n', '<leader>g', builtin.git_files, {})
 vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 vim.keymap.set('n', 'gb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fc', builtin.commands, {})
