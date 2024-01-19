bec028adae9eed16ecef2ded3fbee28ddda1a284
Den Tsou <dtsou@atlassian.com>
2024-01-19 14:57:46 +1100

    add copilot

--- lua/den/lazy.lua
+++ lua/den/lazy.lua
@@ -1,56 +1,59 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { 'folke/tokyonight.nvim' },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-  -- or                            , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-  { 'tpope/vim-fugitive' },
-  {
-    'VonHeikemen/lsp-zero.nvim', branch = 'v3.x',
-    dependencies = {
-      -- LSP Support
-      {
-        'neovim/nvim-lspconfig', -- Required
-        -- this can be enabled on neovim 0.10 is used
-        -- opts = {
-        --   inlay_hints = { enabled = true }
-        -- }
-      },
-      {'williamboman/mason.nvim'},           -- Optional
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    { 'folke/tokyonight.nvim' },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        -- or                            , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    { 'nvim-treesitter/nvim-treesitter',          build = ':TSUpdate' },
+    { 'tpope/vim-fugitive' },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        dependencies = {
+            -- LSP Support
+            {
+                'neovim/nvim-lspconfig', -- Required
+                -- this can be enabled on neovim 0.10 is used
+                -- opts = {
+                --   inlay_hints = { enabled = true }
+                -- }
+            },
+            { 'williamboman/mason.nvim' },   -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},         -- Required
-      {'hrsh7th/cmp-nvim-lsp'},     -- Required
-      {'hrsh7th/cmp-buffer'},       -- Optional
-      {'hrsh7th/cmp-path'},         -- Optional
-      {'saadparwaiz1/cmp_luasnip'}, -- Optional
-      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'hrsh7th/cmp-buffer' }, -- Optional
+            { 'hrsh7th/cmp-path' }, -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},             -- Required
-      {'rafamadriz/friendly-snippets'}, -- Optional
-    }
-  },
-  { 'windwp/nvim-ts-autotag' },
-  -- probably install harpoon?
-  -- probably install undotree?
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },     -- Required
+            { 'rafamadriz/friendly-snippets' }, -- Optional
+        }
+    },
+    { 'github/copilot.vim' },
+    { 'windwp/nvim-ts-autotag' },
+    -- probably install harpoon?
+    -- probably install undotree?
 })
 
 -- TODO: find a better place for this
