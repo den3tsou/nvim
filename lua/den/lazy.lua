184850bd6da6a399bfef7939f940d436b97dbfcd
Den Tsou <den3tsou@gmail.com>
2024-11-05 20:02:17 +1100

    add surround

--- lua/den/lazy.lua
+++ lua/den/lazy.lua
@@ -63,6 +63,16 @@ require("lazy").setup({
         -- install jsregexp (optional!).
         build = "make install_jsregexp",
         dependencies = { "saadparwaiz1/cmp_luasnip", "rafamadriz/friendly-snippets" },
+    },
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
     }
     -- {
     --     "pmizio/typescript-tools.nvim",
