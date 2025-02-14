43cf4c80105d8f4957994220e9ba938c1cf55a3b
Den Tsou <den3tsou@gmail.com>
2025-02-15 09:00:15 +1100

    change all width

--- after/plugin/zen-mode.lua
+++ after/plugin/zen-mode.lua
@@ -1,7 +1,7 @@
 vim.keymap.set('n', '<leader>z', function()
     require("zen-mode").toggle({
         window = {
-            width = .85 -- width will be 85% of the editor width
+            width = .65 -- width will be 65% of the editor width
         }
     })
 end, {})
