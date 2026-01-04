d183ec2efc900965bee0698f94fc1de034b51df1
Den Tsou <den3tsou@gmail.com>
2026-01-04 13:56:31 +1100

    add golangci-lint and good bye ubuntu

--- install_macos.sh
+++ install_macos.sh
@@ -6,4 +6,6 @@ rm -rf ~/.local/state/nvim
 rm -rf /usr/local/bin/nvim
 rm -rf ./plugin/packer_compiled.lua
 
+curl -sSfL https://golangci-lint.run/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.7.2
+
 brew install neovim ripgrep
